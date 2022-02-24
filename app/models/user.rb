da444f8a577447cfab1c7232198c9e8831bb9c0f
anonymous anonymous <paul.park1@kellogg.northwestern.edu>
2022-02-24 05:43:27 +0000

    Add JWT Concern.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  # Direct associations
+  
+  include JwtToken
+# Direct associations
 
   has_many   :messages,
              :foreign_key => "prospective_buyer_id",
