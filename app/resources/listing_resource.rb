30fcc7a7011deb4c63aa027e01b88eaa01ede51d
anonymous anonymous <paul.park1@kellogg.northwestern.edu>
2022-02-24 05:43:18 +0000

    Generate graphiti resources for listings

---
+++ app/resources/listing_resource.rb
@@ -0,0 +1,19 @@
+class ListingResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :seller_id, :integer
+  attribute :title, :string
+  attribute :listing_title, :string
+  attribute :price, :string
+  attribute :category, :string
+  attribute :location, :string
+  attribute :status, :string
+  attribute :buyer_id, :integer
+  attribute :picture, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
